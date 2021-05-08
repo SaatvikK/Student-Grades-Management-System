dd2bd6bba612d3397d12e3a5643f4076a51e6fbe
Saatvik K <48349076+SaatvikK@users.noreply.github.com>
2021-05-08 11:57:07 +0100

    Update main.cs

--- main.cs
+++ main.cs
@@ -3,7 +3,7 @@ using System.IO;
 
 namespace Student_Grades_Management_System {
   class main {
-    static void Main(string[] args){
+    static void Main(string[] args) {
       Console.WriteLine("Starting...");
       Console.WriteLine("Welcome to SGMS, or the Student Grades Management System...");
       Console.WriteLine("Run `help` to see different commands that can be ran!");
@@ -12,10 +12,13 @@ namespace Student_Grades_Management_System {
       string[] ArgsArr = input.Split(" "); //command = args[0]
 
       //Command Handler
-      switch(args[0]) {
+      switch(ArgsArr[0]) {
         case "addstudent":
           student newStudent = new student();
-          newStudent.addMarks();
+          bool hi = newStudent.addMarks();
+          break;
+        default:
+          break;
       }
     }
   }
